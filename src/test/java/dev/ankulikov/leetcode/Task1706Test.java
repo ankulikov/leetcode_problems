328020a0f2f475bb64f2371f8ac3306c1cf62610
andrei.kulikov <andrei.kulikov@revolut.com>
2021-03-04 15:50:32 +0300

    [medium] Task 1706. Where Will the Ball Fall

---
+++ src/test/java/dev/ankulikov/leetcode/Task1706Test.java
@@ -0,0 +1,44 @@
+package dev.ankulikov.leetcode;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+class Task1706Test {
+
+    private final Task1706 task = new Task1706();
+
+    @ParameterizedTest
+    @MethodSource("data")
+    public void should_provide_correct_output(int[][] grid, int[] expected) {
+        // when
+        var actual = task.findBall(grid);
+
+        // then
+        assertArrayEquals(expected, actual);
+    }
+
+    public static Stream<Arguments> data() {
+        return Stream.of(
+            Arguments.of(new int[][]{
+                {1, 1, 1, -1, -1},
+                {1, 1, 1, -1, -1},
+                {-1, -1, -1, 1, 1},
+                {1, 1, 1, 1, -1},
+                {-1, -1, -1, -1, -1}
+            }, new int[]{1, -1, -1, -1, -1}),
+            Arguments.of(new int[][]{{-1}}, new int[]{-1}),
+            Arguments.of(new int[][]{
+                    {1, 1, 1, 1, 1, 1},
+                    {-1, -1, -1, -1, -1, -1},
+                    {1, 1, 1, 1, 1, 1},
+                    {-1, -1, -1, -1, -1, -1}
+                }, new int[]{0, 1, 2, 3, 4, -1}
+            ));
+    }
+
+}
