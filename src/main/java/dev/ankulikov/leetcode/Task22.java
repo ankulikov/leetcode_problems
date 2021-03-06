f35562e452b0de8d360c85a5bc3cd6784f2fa8fe
andrei.kulikov <andrei.kulikov@revolut.com>
2021-03-06 15:58:22 +0300

    [medium] Task 22. Generate Parentheses

---
+++ src/main/java/dev/ankulikov/leetcode/Task22.java
@@ -0,0 +1,54 @@
+package dev.ankulikov.leetcode;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Stream.concat;
+
+/**
+ * Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: n = 3
+ * Output: ["((()))","(()())","(())()","()(())","()()()"]
+ * Example 2:
+ * <p>
+ * Input: n = 1
+ * Output: ["()"]
+ * <p>
+ * <p>
+ * Constraints:
+ * <p>
+ * 1 <= n <= 8
+ *
+ * @see <a href="https://leetcode.com/problems/generate-parentheses/"></a>
+ */
+
+public class Task22 {
+
+        public List<String> generateParenthesis(int n) {
+            return generateParenthesisPartial(0, 0, n)
+                .collect(toList());
+
+        }
+
+        private Stream<String> generateParenthesisPartial(int openedCount, int closedCount, int pairsCount) {
+            if (openedCount == pairsCount && closedCount == pairsCount) {
+                return Stream.of("");
+            }
+            final var withOpenedOrSkipped = openedCount < pairsCount
+                                            ? generateParenthesisPartial(openedCount + 1, closedCount, pairsCount)
+                                                .map(tail -> "(" + tail)
+                                            : Stream.<String>of();
+
+            final var withClosedOrSkipped = openedCount > closedCount
+                                            ? generateParenthesisPartial(openedCount, closedCount + 1, pairsCount)
+                                                .map(tail -> ")" + tail)
+                                            : Stream.<String>of();
+
+            return concat(withOpenedOrSkipped, withClosedOrSkipped);
+        }
+}
---
+++ src/test/java/dev/ankulikov/leetcode/Task22Test.java
@@ -0,0 +1,34 @@
+package dev.ankulikov.leetcode;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Task22Test {
+
+    private final Task22 task = new Task22();
+
+    @ParameterizedTest
+    @MethodSource("data")
+    public void should_provide_correct_output(int pairsCount, List<String> expected) {
+        // when
+        var actual = task.generateParenthesis(pairsCount);
+
+        // then
+        assertEquals(actual, expected);
+    }
+
+    public static Stream<Arguments> data() {
+        return Stream.of(
+            Arguments.of(3, List.of("((()))","(()())","(())()","()(())","()()()")),
+            Arguments.of(1, List.of("()")),
+            Arguments.of(2, List.of("(())", "()()"))
+        );
+    }
+
+}
